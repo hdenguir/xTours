15660d74db858a79c4b29998c2fa109d5e72d3d3
Hafid Denguir <haf2com@gmail.com>
2019-10-16 15:14:12 +0200

    implemented cors

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const xssClean = require('xss-clean');
 const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
 const compression = require('compression');
+const cors = require('cors');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -24,6 +25,11 @@ app.enable('trust proxy');
 
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
+
+// GLOBAL MIDDLEWARES
+// Implement CORS
+app.use(cors()); // Access-Control-Allow-Origin *
+app.options('*', cors());
 
 // Serving static files
 app.use(express.static(path.join(__dirname, 'public')));
